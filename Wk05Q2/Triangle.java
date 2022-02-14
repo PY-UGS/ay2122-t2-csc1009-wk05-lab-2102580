3fc4b883489e68d38d4892285e7ef2cd5122e93f
2102580 <97870265+2102580@users.noreply.github.com>
2022-02-14 20:51:48 +0800

    Add files via upload

---
+++ Wk05Q2/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle extends Shape{
+    
+
+    public Triangle(double dim1, double dim2){
+        super(dim1, dim2);
+    }
+
+    public double area(){
+        System.out.println("Inside Area for Triangle.");
+        return(0.5*getDim1()*getDim2());
+    }
+}
